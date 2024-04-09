b5352bace89f1e662a9f45e950c25ddf2b90325a
kellyxc28 <kellyxiongc@gmail.com>
2024-04-09 12:57:04 -0400

    Completed Question 7: MODIFY - continueGuardClause()

--- src/modify.js
+++ src/modify.js
@@ -3,17 +3,18 @@ const whileToFor = () => {
     console.log(i);
   }
 };
-console.log("Question 6");
-whileToFor();
+// console.log("Question 6");
+// whileToFor();
 
 const continueGuardClause = () => {
   for (let i = 1; i < 5; i++) {
-    if (!(i === 2) && !(i === 3)) {
-      console.log("Sure glad this isn't 2 or 3");
-      console.log(i);
-    }
+    if (i === 2 || i === 3) continue;
+    console.log("Sure glad this isn't 2 or 3");
+    console.log(i);
   }
 };
+console.log("Question 7");
+continueGuardClause();
 
 module.exports = {
   whileToFor,
