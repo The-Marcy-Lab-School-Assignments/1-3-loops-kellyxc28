35d3f8572c62861d081656edc97f02cd760214b3
kellyxc28 <kellyxiongc@gmail.com>
2024-04-09 12:09:29 -0400

    Completed Question 10: fizzbuzz()

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -46,7 +46,23 @@ loopUpToNum();
 
 // Q10 
 const fizzbuzz = () => {
+    for (let i = 1; i <= 100; i++) {
+      if (i % 3 !== 0 && i % 5 !== 0) {
+        console.log(i);
+      }
+      else if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz');
+      }
+      else if (i % 3 === 0 && i % 5 !== 0) {
+        console.log('fizz');
+      }
+      else if (i % 3 !== 0 && i % 5 === 0) {
+        console.log('buzz');
+      }
+    }
 };
+console.log("Question 10");
+fizzbuzz();
 
 module.exports = {
   loop0UpTo10,
