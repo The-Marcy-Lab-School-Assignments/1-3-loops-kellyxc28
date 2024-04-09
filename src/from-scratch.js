1e1052fb8ec4d4940f7ac734c4695188f907f463
kellyxc28 <kellyxiongc@gmail.com>
2024-04-09 11:48:22 -0400

    Completed Question 4: countdown5to0()

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,3 +1,4 @@
+// Q1 
 const loop0UpTo10 = () => {
     for (let i = 0; i < 10; i++) {
       console.log(i);
@@ -6,6 +7,7 @@ const loop0UpTo10 = () => {
 console.log("Question 1");
 loop0UpTo10();
 
+// Q2 
 const loop5to10 = () => {
   for (let i = 5; i <= 10; i++) {
     console.log(i);
@@ -14,6 +16,7 @@ const loop5to10 = () => {
 console.log("Question 2");
 loop5to10();
 
+// Q3
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
     for (let i = 0; i < 10; i+=2) { // incrementing by 2 with STARTING at 0 => only even values 
@@ -24,12 +27,20 @@ const loopEvenNumbersUpTo10 = () => {
 console.log("Question 3");
 loopEvenNumbersUpTo10();
 
+// Q4 
 const countdown5to0 = () => {
+    for (let i = 5; i >= 0; i--) {
+      console.log(i);
+    }
 };
+console.log("Question 4");
+countdown5to0();
 
+// Q5  
 const loopUpToNum = () => {
 };
 
+// Q10 
 const fizzbuzz = () => {
 };
 
